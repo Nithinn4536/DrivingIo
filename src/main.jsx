567b466a6a2571dff1da677f85317f8f7a0ea966
Nithinn4536 <knithin4536@gmail.com>
2025-08-09 22:19:33 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-mport React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 // The import for a global CSS file has been removed as it was causing a build error
